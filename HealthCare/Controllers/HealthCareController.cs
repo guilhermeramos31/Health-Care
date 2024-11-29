85717fada8d4998904632399f81373f8961521f3
guilhermeramos31 <guilhermeramos31.gr@gmail.com>
2024-11-29 20:56:36 -0300

    first commit

---
+++ HealthCare/Controllers/HealthCareController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthCareController.Controllers
+{
+    [ApiController]
+    [Route( "[controller]" )]
+    public class HealthCareController : ControllerBase
+    {
+        public HealthCareController()
+        {
+        }
+    }
+}

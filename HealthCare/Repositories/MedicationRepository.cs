315f2200f1e585f3548c86c5a952cced367004cb
Wagner Ramos <guilhermeramos31.gr@gmail.com>
2025-02-13 14:46:03 -0300

    feat: repositorie medication

---
+++ HealthCare/Repositories/MedicationRepository.cs
@@ -0,0 +1,46 @@
+﻿using HealthCare.Infrastructure.Data.Context;
+using HealthCare.Models.MedicationEntity;
+using HealthCare.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthCare.Repositories;
+
+public class MedicationRepository(HealthCareContext dbContext) : IMedicationRepository
+{
+    public async Task<Medication> Create(Medication newMedication)
+    {
+        var medication = await dbContext.Medications.AddAsync(newMedication);
+        return medication.Entity;
+    }
+
+    public Medication Update(Medication updateMedication)
+    {
+        var medication = dbContext.Medications.Update(updateMedication);
+        return medication.Entity;
+    }
+
+    public void Delete(Medication medication)
+    {
+        dbContext.Medications.Remove(medication);
+    }
+
+    public async Task<Medication?> GetMedicationById(Guid medicationId)
+    {
+        return await dbContext.Medications.FindAsync(medicationId);
+    }
+
+    public Task<List<Medication>> GetAllMedications(Guid patientId, int? pageSize, int? pageNumber)
+    {
+        var query = dbContext.Medications.Where(x => x.Id == patientId);
+        if (pageSize.HasValue && pageNumber.HasValue && pageSize.Value > 0 && pageNumber > 0)
+        {
+            query = query.Skip((pageNumber.Value - 1) * pageSize.Value)
+                .Take(pageSize.Value);
+        }
+
+        query = query.Include(hs => hs.Patient)
+            .OrderByDescending(x => x.CreateAt);
+
+        return query.ToListAsync();
+    }
+}
